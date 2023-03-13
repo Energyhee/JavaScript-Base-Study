1694272acf694bba0b36e54cace2eaa4b95c3bec
hee <nergyhee@naver.com>
2023-03-13 11:08:36 +0900

    WRITE : 번호판등록 프로세스 퍼블 고도화

--- 01. Java Script/05.모음/past/번호판등록프로세스/js/custom.js
+++ 01. Java Script/05.모음/past/번호판등록프로세스/js/custom.js
@@ -6,9 +6,7 @@ const _Fn = {
 
             tBtn.forEach((bEl, idx) => {
                 bEl.addEventListener(event, (thisEl) => {
-                    // # btn
                     _Fn.setClass(bEl, 'active');
-                    // # cont
                     _Fn.setClass(tCont[idx], 'on');
                 });
             });
@@ -21,15 +19,12 @@ const _Fn = {
             let target = (typeof scope == 'string') ? document.querySelectorAll(scope)[0] : scope;
             switch (type) {
                 default : 
-                    // # toggle
                     (target.classList.contains(name) != true) ? target.classList.add(name) : target.classList.remove(name);
                     break;
                 case 'add' : 
-                    // # add
                     if(target.classList.contains(name) != true) target.classList.add(name);
                     break;
                 case 'remove' : 
-                    // # remove
                     if(target.classList.contains(name) === true) target.classList.remove(name);
                     break;
             }
@@ -39,4 +34,35 @@ const _Fn = {
     }
 };
 
-_Fn.setEvent('.toggle-btn', '.toggle-cont', 'click');   // # obj, cont, event
+$(document).ready(function(){
+    _Fn.setEvent('.toggle-btn', '.toggle-cont', 'click');
+
+    let datePicker = document.querySelector('#datepicker'),
+        dateTimePicker = document.querySelector('#datetimepicker');
+
+    if(datePicker){
+        $('#datepicker').datepicker({
+            dateFormat: 'yy-mm-dd',
+            closeText: '취소',
+            changeYear: true,
+            changeMonth: true,
+            showButtonPanel: true,
+            monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+            dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'],
+            onClose : () => {
+                if ( window.event.target.classList.contains('ui-datepicker-close') ) {
+                    document.querySelector('#datepicker').value = '';
+                }
+            },
+        });
+    }
+
+    if(dateTimePicker){
+        $('#datetimepicker').datetimepicker({
+            allowTimes: ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'],
+            datepicker: false,
+            format: 'H:i',
+            lang: 'kr',
+        });
+    }
+});
