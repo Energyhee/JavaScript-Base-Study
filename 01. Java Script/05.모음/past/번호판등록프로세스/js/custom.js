9cba95a4ca21900822d551cbadc3d335554b033a
hee <nergyhee@naver.com>
2023-03-14 17:53:21 +0900

    WRITE : 번호판등록프로세스 java script 수정

--- 01. Java Script/05.모음/past/번호판등록프로세스/js/custom.js
+++ 01. Java Script/05.모음/past/번호판등록프로세스/js/custom.js
@@ -1,17 +1,25 @@
 const _Fn = {
-    setEvent : (btn, scope, event) => {
+    setPaste : (obj) => {
         try{
-            let tBtn = document.querySelectorAll(btn),
-                tCont = document.querySelectorAll(scope);
+            let inp = obj.querySelectorAll('input'),
+                val = '';
 
-            tBtn.forEach((bEl, idx) => {
-                bEl.addEventListener(event, (thisEl) => {
-                    _Fn.setClass(bEl, 'active');
-                    _Fn.setClass(tCont[idx], 'on');
-                });
+            inp.forEach((el) => {
+                let type = el.getAttribute('type');
+                
+                switch (type) {
+                    default : 
+                        if (el.checked === true) val = el.value;
+                        break;
+                    case 'text' : 
+                        if (el.value.length > 0) val = el.value;
+                        break;
+                }
             });
+
+            return val;
         }catch{
-            console.log('※ _Fn.setEvent ERROR');
+            console.log('※ _Fn.getValue Error');
         }
     },
     setClass : (scope, name, type) => {
@@ -29,16 +37,108 @@ const _Fn = {
                     break;
             }
         }catch{
-            console.log('※ _Fn.setClass ERROR');
+            console.log('※ _Fn.setClass Error');
+        }
+    },
+    valReset : (wrap, el) => {
+        try{
+            let rObj = wrap.querySelectorAll('input[type="radio"]'),
+                tObj = wrap.querySelector('input[type="text"]');
+
+            if (el.getAttribute('type') === 'radio' && tObj.value.length > 0){
+                tObj.value = '';
+                _Fn.setClass(tObj.closest('.last'), 'focus');
+            }else if(el.getAttribute('type') === 'text'){
+                rObj.forEach((obj) => {
+                    if ( obj.checked === true ) obj.checked = false;
+                });
+            }
+        }catch{
+            console.log('※ _Fn.valReset Error');
+        }
+    },
+    dataResult : () => {
+        try{
+            let dataSpan = document.querySelectorAll('.sel-data'),
+                dataArr = [];
+
+            dataSpan.forEach((span) => {
+                dataArr.push(span.children[0].innerText);
+            })
+            
+            return dataArr;
+        }catch{
+            console.log('※ _Fn.dataResult Error');
         }
     }
 };
 
-$(document).ready(function(){
-    _Fn.setEvent('.toggle-btn', '.toggle-cont', 'click');
+document.addEventListener('DOMContentLoaded', function(){
+    const datePicker = document.querySelector('#datepicker'),
+          dateTimePicker = document.querySelector('#datetimepicker'),
+          toggleCont = document.querySelector('.toggle-wrap');
 
-    let datePicker = document.querySelector('#datepicker'),
-        dateTimePicker = document.querySelector('#datetimepicker');
+    if(toggleCont){
+        const tBtn = toggleCont.querySelectorAll('.toggle-btn'),
+              tCont = toggleCont.querySelectorAll('.toggle-cont');
+
+        tBtn.forEach((bEl, idx) => {
+            bEl.addEventListener('click', () => {
+                let dataEl = bEl.querySelector('.sel-data');
+
+                _Fn.setClass(bEl, 'active');
+                _Fn.setClass(tCont[idx], 'on');
+
+                // 데이터 받아서 넣기
+                if (dataEl != null && _Fn.setPaste(tCont[idx]).length > 0) {
+                    dataEl.style.display = 'flex';
+                    dataEl.children[0].innerText = _Fn.setPaste(tCont[idx]);
+                }else{
+                    dataEl.style.display = 'none';
+                }
+                if ( bEl.classList.contains('scrollNone') != true ){
+                    window.scrollTo({ 
+                        top : bEl.offsetTop, 
+                        behavior : 'smooth' 
+                    });
+                }
+            });
+        });
+
+        const mixInp = toggleCont.querySelector('.mixInp'),
+              mixText = mixInp.querySelector('input[type="text"]'),
+              mixRadio = mixInp.querySelector('input[type="text"]');
+        
+        document.querySelectorAll('input').forEach((inp) => {
+            if (inp.getAttribute('type')  === 'radio' || inp.getAttribute('type')  === 'checkbox' ){
+                // 데이터 받아서 넣기
+                inp.addEventListener('click', (event) => {
+                    let val = event.target.value,
+                        span = event.target.closest('li').querySelector('.sel-data');
+    
+                    span.children[0].innerText = val;
+                    _Fn.valReset(mixInp, event.target);
+                });
+            }else if(inp.getAttribute('type')  === 'text'){
+                // 데이터 받아서 넣기
+                inp.addEventListener('input', (event) => {
+                    let val = event.target.value,
+                        span = event.target.closest('li').querySelector('.sel-data');
+    
+                    span.children[0].innerText = val;
+                    _Fn.valReset(mixInp, event.target);
+                });
+            }
+        });
+
+        mixText.addEventListener('focus', (event) => {
+            _Fn.valReset(mixInp, event.target);
+            _Fn.setClass(event.target.closest('.last'), 'focus', 'add');
+        })
+        mixText.addEventListener('blur', (event) => {
+            if ( event.target.value.length < 1 ) _Fn.setClass(event.target.closest('.last'), 'focus', 'remove');
+        });
+    }
 
     if(datePicker){
         $('#datepicker').datepicker({
@@ -51,7 +151,7 @@ $(document).ready(function(){
             dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'],
             onClose : () => {
                 if ( window.event.target.classList.contains('ui-datepicker-close') ) {
-                    document.querySelector('#datepicker').value = '';
+                    datePicker.value = '';
                 }
             },
         });
