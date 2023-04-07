bd4fe008774d8653cd684d2fdf94552c6a6972d3
hee <nergyhee@naver.com>
2023-04-07 17:31:45 +0900

    WRITE : 삽질 레츠고!

---
+++ 01. Java Script/05.모음/past/AddDelCont/README.md
@@ -0,0 +1,12 @@
+# ✍️ Java Script
+
+<br />
+
+>   * 실무 작업을 진행하면서 _**접근**_
+
+>       code --list-extensions > extensions.list
+>       cat extensions.list |% { code --install-extension $_}
+
+<br />
+
+#### ● [강화시도 시뮬레이터](http://nergyhee.dothome.co.kr/function/past/luckTest/)
---
+++ 01. Java Script/05.모음/past/AddDelCont/sources/js/custom.js
@@ -0,0 +1,25 @@
+const log = console.log;
+const _Fn = {
+    setClass : (scope, name, type) => {
+        try{
+            let target = (typeof scope == 'string') ? document.querySelectorAll(scope)[0] : scope;
+            switch (type) {
+                default : 
+                    (target.classList.contains(name) != true) ? target.classList.add(name) : target.classList.remove(name);
+                    break;
+                case 'add' : 
+                    if(target.classList.contains(name) != true) target.classList.add(name);
+                    break;
+                case 'remove' : 
+                    if(target.classList.contains(name) === true) target.classList.remove(name);
+                    break;
+            }
+        }catch(error){
+            log('※ _Fn.setClass Error \n\n', error);
+        }
+    },
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+
+});
